121c8fc8affbbbfef629311ec705ae6a964f0d40
SuhBrandon <suhbrandon123@gmail.com>
2024-11-10 09:01:20 -0800

    added image

---
+++ lib/pages/auth/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToLogin();
+  }
+
+  _navigateToLogin() async {
+    await Future.delayed(Duration(seconds: 3), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage("http://bit.ly/3YZofmz")), // Add your background image here
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: CircularProgressIndicator(),
+          ),
+        ],
+      ),
+    );
+  }
+}
